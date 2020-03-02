43996fa472017621931c8515961603c28ff02a1c
petziferum <daboarderpjb@gmail.com>
2020-03-02 18:14:24 +0100

    initiate app bar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app >
+      <v-app-bar-nav-icon @click.native="sideNav = !sideNav" class="hidden-sm-and-up"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-btn flat class="hidden-xs-only"><v-icon left>mdi-account</v-icon>View Meetups</v-btn>
+    </v-app-bar>
+    <v-navigation-drawer app v-model="sideNav">
+
+      <v-list-item-tile>
+        <v-list-item-action><v-icon>mdi-account</v-icon></v-list-item-action>
+        <v-list-item-action-text>View Meetups</v-list-item-action-text>
+      </v-list-item-tile>
+    </v-navigation-drawer>
+    <v-content>
+      <h1>Home</h1>
+    </v-content>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+  name: 'App',
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  components: {
+  },
+
+  data: () => ({
+    sideNav: false,
+  }),
+};
+</script>
