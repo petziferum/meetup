599bea6278ad2c92ee7f6c2fde15fe7ac8a4bfff
petziferum <daboarderpjb@gmail.com>
2020-03-03 09:22:26 +0100

    added remote

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <v-app>
     <v-app-bar app >
-      <v-app-bar-nav-icon @click.native="sideNav = !sideNav" class="hidden-sm-and-up"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click.native="sideNav = !sideNav" class="hidden-sm-and-down"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
-      <v-btn flat class="hidden-xs-only"><v-icon left>mdi-account</v-icon>View Meetups</v-btn>
+      <v-btn text class="hidden-xs-only""><v-icon left>mdi-account</v-icon>View Meetups</v-btn>
     </v-app-bar>
     <v-navigation-drawer app v-model="sideNav">
-
-      <v-list-item-tile>
-        <v-list-item-action><v-icon>mdi-account</v-icon></v-list-item-action>
-        <v-list-item-action-text>View Meetups</v-list-item-action-text>
-      </v-list-item-tile>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
     <v-content>
       <h1>Home</h1>
