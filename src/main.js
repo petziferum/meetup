bbd4839717cecd9a6abacd7d4c27c034cc32e42c
petziferum <daboarderpjb@gmail.com>
2020-03-15 20:12:03 +0100

    firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import * as firebase from 'firebase'
 
 Vue.config.productionTip = false
 
@@ -10,5 +11,16 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    firebase.initializeApp({
+        apiKey: "AIzaSyCwpWD-zssl_8wwXNPu8WkIC84qQ4_4YHY",
+        authDomain: "meetup-edd70.firebaseapp.com",
+        databaseURL: "https://meetup-edd70.firebaseio.com",
+        projectId: "meetup-edd70",
+        storageBucket: "meetup-edd70.appspot.com",
+        messagingSenderId: "592274862907",
+        appId: "1:592274862907:web:36344466cd8a4bb93f3d1e"
+        })
+  }
 }).$mount('#app')
