2d051c7c64180f062dc754ac179bf91d1674749e
Petzi <daboarderpjb@gmail.com>
2020-03-20 14:24:34 +0100

    added breakpoints

--- src/components/meetup/Meetups.vue
+++ src/components/meetup/Meetups.vue
@@ -1,15 +1,15 @@
 <template>
     <v-container>
-        <v-row class="mb-2" height="100px" v-for="meetup in meetups" :key="meetup.id" style="border:1px solid">
-            <v-col>
+        <v-row class="mb-2" v-for="meetup in meetups" :key="meetup.id" >
+            <v-col cols="12">
                 <v-skeleton-loader class="" type="card" max-width="100%" v-if="loading"></v-skeleton-loader>
-                <v-card  shaped class="mx-auto pa-0" width="100%"  height="200px" style="overflow:hidden; cursor:pointer;" color="white">
+                <v-card  shaped class="mx-auto pa-0" width="100%"  style="overflow:hidden; cursor:pointer;" color="white">
                     <v-container class="ma-0 pa-0">
-                        <v-row class="ma-0 pa-0">
-                            <v-col cols="6" class="ma-0 pa-0"  style="border:1px solid">
-                                <v-img aspect-ratio="3" cover :src="meetup.imgsrc"></v-img>
+                        <v-row no-gutters class="ma-0 pa-0">
+                            <v-col cols="12" sm="12" md="6" lg="6" class="ma-0 pa-0"  style="border:1px solid">
+                                <v-img aspect-ratio="1.5" cover :src="meetup.imgsrc"></v-img>
                             </v-col>
-                            <v-col cols="6" style="border:1px solid">
+                            <v-col cols="12" sm="12" md="6" lg="6" style="border:1px solid">
                                 <v-card-title>{{meetup.title}}</v-card-title>
                                 <v-card-text>{{meetup}}</v-card-text>
                                 <v-card-actions>
