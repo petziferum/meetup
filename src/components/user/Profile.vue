38cf56f1340ba2719ec9544ae6de9998dc09fff6
Petzi <daboarderpjb@gmail.com>
2020-04-14 22:57:30 +0200

    test auf Profilseite

--- src/components/user/Profile.vue
+++ src/components/user/Profile.vue
@@ -1,6 +1,6 @@
 <template>
-    <v-container>
-        <v-row align="justify-center">
+    <v-container full-width style="border: 1px solid">
+        <v-row align="center">
             <v-col cols="6">
                 <v-text-field v-model="gesamt" label="Gesamtbevölkerung"></v-text-field>
                 <v-text-field v-model="infizierte" label="Infizierte"></v-text-field>
@@ -12,11 +12,26 @@
                 Prozentual Infizierte: {{ zahl }} %
             </v-col>
         </v-row>
-        <v-row align="justify-center">
+        <v-row align="center">
             <v-col cols="6">
                 <app-card><h3 slot="title">Das sollte im Titleslot stehen</h3>
                 <v-card-text slot="text">Das ist im Slot Text</v-card-text>
                 </app-card>
+            </v-col>
+        </v-row>
+        <v-row align="center" justify="center">
+            <v-col cols="12">
+                <form>
+                    <v-text-field v-model="autobrand" label="Marke"></v-text-field>
+                    <v-text-field v-model="weight" label="Gewicht"></v-text-field>
+                    <v-text-field v-model="speed" label="Höchstgeschwindigkeit"></v-text-field>
+                    <v-btn @click="autoErstellen(autobrand,weight, speed)">doSomething</v-btn>
+                <v-btn @click="infizierte = 0">null</v-btn>
+                </form>
+
+
+                <v-alert color="error">{{feedback}}</v-alert>
+                {{autos}}
             </v-col>
         </v-row>
     </v-container>
@@ -30,20 +45,40 @@
             appCard
         },
         data:() => ({
-            infizierte:'',
-            gesamt:'',
+            infizierte:0,
+            gesamt:0,
             prozent: 0,
+            feedback: '',
+            autobrand:'',
+            weight: '',
+            speed:'',
+            autos: []
 
         }),
         methods: {
+            autoErstellen(name,weight,speed) {
+                class Car {
+                    constructor(brand, weight, speed) {
+                        this.carname = brand;
+                        this.gewicht = weight;
+                        this.geschwindigkeit = speed;
+                    }
+                }
+                var auto = new Car(name,weight,speed)
+                this.autos.push(auto)
+                this.autobrand = ''
+                this.weight=''
+                this.speed=''
+
+            },
 
         },
         computed: {
             zahl() {
                 let einwohner = this.gesamt
                 let infizierte = this.infizierte
-                const ergebniss =  infizierte/einwohner *100
-                return ergebniss.toFixed(4)
+                   let ergebniss = infizierte / einwohner * 100
+                return ergebniss.toFixed(2)
             },
 
         }
